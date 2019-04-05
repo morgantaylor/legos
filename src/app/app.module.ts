ed5b71636f738e7fb4b56cd3de7a2ae48c70b6b1
Morgan Taylor <mtwebmedia@gmail.com>
2019-04-04 23:22:04 -0700

    Add in button components and sub components
    - buttons
    - states
    - icon
    - groups
    - dropdowns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ButtonsComponent } from './buttons/buttons.component';
+import { IconComponent } from './buttons/icon/icon.component';
+import { StatesComponent } from './buttons/states/states.component';
+import { GroupsComponent } from './buttons/groups/groups.component';
+import { DropdownsComponent } from './buttons/dropdowns/dropdowns.component';
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent
+    ButtonsComponent,
+    IconComponent,
+    StatesComponent,
+    GroupsComponent,
+    DropdownsComponent
   ],
   imports: [
     BrowserModule,

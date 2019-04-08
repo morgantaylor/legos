f8d318f305958c3a7e681a57e4ec2f4e32661eac
Morgan Taylor <mtwebmedia@gmail.com>
2019-04-07 19:46:33 -0700

    Icon section

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-icons',
+  selector: 'icons',
   templateUrl: './icons.component.html',
   styleUrls: ['./icons.component.scss']
 })

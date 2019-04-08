6788e6d17964cf52d8abd3cbecd59840fc48f88d
Morgan Taylor <mtwebmedia@gmail.com>
2019-04-07 20:31:39 -0700

    Utilities, atoms, and components are in

---
+++ src/app/chips/chips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'chips',
+  templateUrl: './chips.component.html',
+  styleUrls: ['./chips.component.scss']
+})
+export class ChipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

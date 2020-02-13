2aebe39feb09c583e34c13012ad8525ba0642e39
Morgan Taylor <mtwebmedia@gmail.com>
2020-02-12 17:36:57 -0800

    Set flag for UI view to see staging is enabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'legos';
+
+  	constructor() {
+  		if (environment.staging) {
+  			console.log('staging: ', environment.staging);
+    		console.log('production: ', environment.production);
+    	}
+  	}
+
+  	title = 'LegoCSS';
+  	stagingFlag = environment.staging;
 }
